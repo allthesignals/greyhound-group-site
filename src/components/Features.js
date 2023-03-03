eeda758495c4df018fd981302459c141f1760646
Matt Gardner <wilburnforce@gmail.com>
2023-03-03 15:53:09 -0500

    Use pseudoelements

--- src/components/Features.js
+++ src/components/Features.js
@@ -5,12 +5,29 @@ import PreviewCompatibleImage from "../components/PreviewCompatibleImage";
 const FeatureGrid = ({ gridItems }) => (
   <div className="flex flex-col -translate-y-28 max-w-12 px-32">
     {gridItems.map((item, index) => (
-      <div key={item.text} className={`flex relative pb-12 ${(index % 2 === 0 ? 'self-end' : 'self-start')}`}>
-        <div className="absolute left-4 -top-4 bg-gg-off-white rounded-3xl border-solid border-gg-light-green border-8 max-w-screen-md">
+      <div key={item.text} className={`${(index % 2 === 0 ? 'self-end' : 'self-start')}`}>
+        <div className="
+          relative
+          bg-gg-off-white
+          rounded-3xl
+          border-solid
+          border-gg-light-green
+          border-8
+          max-w-screen-md
+          sm:after:bg-gg-light-gray
+          sm:after:rounded-3xl
+          sm:after:border-solid
+          sm:after:border-gg-dark-green
+          sm:after:border-8
+          sm:after:absolute
+          sm:after:w-full
+          sm:after:h-full
+          sm:after:right-8
+          sm:after:top-8
+          sm:after:-z-10
+          sm:after:content-['']
+        ">
           <p className="p-2 text-3xl">{item.text}</p>
-        </div>
-        <div className="bg-gg-light-gray rounded-3xl border-solid border-gg-dark-green border-8 max-w-screen-md">
-          <p className="p-2 text-3xl invisible">{item.text}</p>
         </div>
       </div>
     ))}
