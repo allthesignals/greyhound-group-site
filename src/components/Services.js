38ffc5ff237ef4b4220554b4246a2733ccf03e7d
Matt Gardner <wilburnforce@gmail.com>
2023-03-20 19:00:15 -0400

    Style services up a little more

--- src/components/Services.js
+++ src/components/Services.js
@@ -4,7 +4,8 @@ import PropTypes from "prop-types";
 const FeatureGrid = ({ gridItems }) => (
   <>
     {gridItems.map((item, index) => ((index % 2 === 0) ?
-      <div className="pl-12 py-4 bg-gg-off-white"><div className="
+      <div className="pl-12 py-4 bg-gg-off-white">
+        <div className="
         bg-gg-light-gray
         border-y-8
         border-l-8
@@ -15,9 +16,18 @@ const FeatureGrid = ({ gridItems }) => (
         w-1/2
         transition-all
         ease-in
-        hover:w-full
-      ">{item.name}</div></div> :
-      <div className="pr-12 py-4 bg-gg-off-white text-right"><div className="
+        hover:w-11/12
+        pt-2
+        group
+      ">
+        <div className="transition-all -translate-x-2/3 group-hover:transform-none group-hover:text-center uppercase text-gg-dark-green font-black text-3xl">
+          <span className="bg-gg-light-gray p-4 rounded-3xl border-8 border-gg-dark-green">{item.name}</span>
+        </div>
+        <div className="invisible group-hover:visible text-right pr-12">{item.description}</div>
+      </div>
+    </div> :
+    <div className="pr-12 py-4 bg-gg-off-white text-right">
+      <div className="
         bg-gg-light-gray
         border-y-8
         border-r-8
@@ -28,8 +38,16 @@ const FeatureGrid = ({ gridItems }) => (
         w-1/2
         transition-[width]
         ease-in
-        hover:w-full
-      ">{item.name}</div></div>
+        hover:w-11/12
+        pt-2
+        group
+      ">
+        <div className="transition-all translate-x-2/3 group-hover:transform-none group-hover:text-center uppercase text-gg-dark-green font-black text-3xl">
+          <span className="bg-gg-light-gray p-4 rounded-3xl border-8 border-gg-dark-green">{item.name}</span>
+        </div>
+        <div className="invisible group-hover:visible text-right pr-12">{item.description}</div>
+      </div>
+    </div>
     ))}
   </>
 );
