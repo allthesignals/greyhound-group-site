2448ba20741e6bfe95c5757d072faec3e2c7bc25
Matt Gardner <wilburnforce@gmail.com>
2023-03-23 22:03:51 -0400

    Finalize styles

--- src/components/Services.js
+++ src/components/Services.js
@@ -9,13 +9,48 @@ const FeatureGrid = ({ gridItems }) => (
 
       return <div
         key={item.text}
-        className={`flex w-full items-start group space-x-10 ${isEven ? "flex-row-reverse self-end space-x-reverse" : "flex-row self-start"}`}
+        className={`
+          flex w-full
+          items-start
+          group
+          relative
+          ${isEven ? 'flex-row-reverse self-end space-x-reverse' : 'flex-row self-start'}
+        `}
       >
+        <div className={`
+          absolute
+          top-0
+          w-full
+        `}>
+          <div className={`
+            transition-all
+            duration-700
+            bg-gg-light-gray
+            p-4
+            uppercase
+            rounded-3xl
+            border-8
+            border-gg-dark-green
+            text-gg-dark-green
+            font-black
+            text-3xl
+            group-hover:border-transparent
+            group-hover:bg-transparent
+            w-fit
+            absolute
+            ${isEven ? `
+              left-1/4
+              group-hover:left-[45%]
+            ` : `
+              left-[60%]
+              group-hover:left-1/4
+            `}
+          `}>{item.name}</div>
+        </div>
         <div
           className={`
             basis-[45%]
             group-hover:basis-[80%]
-            ${isEven ? "rounded-l-3xl border-l-8" : "border-r-8 rounded-r-3xl"}
             bg-gg-light-gray
             border-y-8
             p-12
@@ -23,41 +58,16 @@ const FeatureGrid = ({ gridItems }) => (
             transition-all
             duration-700
             ease-in
-            relative
             box-border
+            ${isEven ? 'rounded-l-3xl border-l-8' : 'border-r-8 rounded-r-3xl'}
+            flex
+            flex-row
           `}
         >
-          <div className={`
-            transition-all
-            uppercase
-            text-gg-dark-green
-            font-black
-            text-3xl
-            w-fit
-            absolute
-            top-0
-          `}>
-            <div className={`
-              bg-gg-light-gray
-              p-4
-              rounded-3xl
-              border-8
-              border-gg-dark-green
-              -translate-y-2
-              group-hover:border-0
-              group-hover:translate-y-0
-              ${isEven ? `
-                -translate-x-[130%]
-                group-hover:translate-x-[120%]
-              ` : `
-                translate-x-[230%]
-                group-hover:translate-x-[120%]
-              `}
-              duration-700
-            `}>{item.name}</div>
+          <div className="inline-block">
+            <PreviewCompatibleImage imageInfo={item} />
           </div>
-          <PreviewCompatibleImage imageInfo={item} />
-          <div className="invisible group-hover:visible text-right pr-12">
+          <div className="invisible group-hover:visible text-left pl-12 pt-12">
             {item.description}
           </div>
         </div>
