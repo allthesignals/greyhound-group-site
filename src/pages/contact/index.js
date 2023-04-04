9e5d747dbde68a8d79395d997be83bc6e39aab3c
Matt Gardner <wilburnforce@gmail.com>
2023-04-03 22:18:56 -0400

    Drop us al ine

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { navigate } from "gatsby-link";
 import Layout from "../../components/Layout";
+import Hero from "../../components/Hero";
 
 function encode(data) {
   return Object.keys(data)
@@ -36,79 +37,98 @@ export default class Index extends React.Component {
   render() {
     return (
       <Layout>
-        <section className="section">
-          <div className="container">
-            <div className="content">
-              <h1>Contact</h1>
-              <form
-                name="contact"
-                method="post"
-                action="/contact/thanks/"
-                data-netlify="true"
-                data-netlify-honeypot="bot-field"
-                onSubmit={this.handleSubmit}
-              >
-                {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
-                <input type="hidden" name="form-name" value="contact" />
-                <div hidden>
-                  <label>
-                    Don’t fill this out:{" "}
-                    <input name="bot-field" onChange={this.handleChange} />
-                  </label>
-                </div>
-                <div className="field">
-                  <label className="label" htmlFor={"name"}>
-                    Your name
-                  </label>
-                  <div className="control">
-                    <input
-                      className="input"
-                      type={"text"}
-                      name={"name"}
-                      onChange={this.handleChange}
-                      id={"name"}
-                      required={true}
-                    />
-                  </div>
-                </div>
-                <div className="field">
-                  <label className="label" htmlFor={"email"}>
-                    Email
-                  </label>
-                  <div className="control">
-                    <input
-                      className="input"
-                      type={"email"}
-                      name={"email"}
-                      onChange={this.handleChange}
-                      id={"email"}
-                      required={true}
-                    />
-                  </div>
-                </div>
-                <div className="field">
-                  <label className="label" htmlFor={"message"}>
-                    Message
-                  </label>
-                  <div className="control">
-                    <textarea
-                      className="textarea"
-                      name={"message"}
-                      onChange={this.handleChange}
-                      id={"message"}
-                      required={true}
-                    />
-                  </div>
-                </div>
-                <div className="field">
-                  <button className="button is-link" type="submit">
-                    Send
-                  </button>
-                </div>
-              </form>
+        <Hero />
+        <div className="
+          bg-white
+          rounded-3xl
+          border-solid
+          border-gg-dark-green
+          border-8
+          p-2
+          m-2
+          mx-24
+        ">
+          <div className="
+            text-gg-ocean-green
+            font-black
+            text-3xl
+            bg-gg-light-gray
+            rounded-3xl
+            border-8
+            border-gg-dark-green
+            p-8
+            w-fit
+            mx-auto
+            -translate-y-12
+            uppercase
+          ">Drop us a line</div>
+          <form
+            name="contact"
+            method="post"
+            action="/contact/thanks/"
+            data-netlify="true"
+            data-netlify-honeypot="bot-field"
+            onSubmit={this.handleSubmit}
+          >
+            {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+            <input type="hidden" name="form-name" value="contact" />
+            <div hidden>
+              <label>
+                Don’t fill this out:{" "}
+                <input name="bot-field" onChange={this.handleChange} />
+              </label>
             </div>
-          </div>
-        </section>
+            <div className="field">
+              <label className="label" htmlFor={"name"}>
+                Your name
+              </label>
+              <div className="control">
+                <input
+                  className="input"
+                  type={"text"}
+                  name={"name"}
+                  onChange={this.handleChange}
+                  id={"name"}
+                  required={true}
+                />
+              </div>
+            </div>
+            <div className="field">
+              <label className="label" htmlFor={"email"}>
+                Email
+              </label>
+              <div className="control">
+                <input
+                  className="input"
+                  type={"email"}
+                  name={"email"}
+                  onChange={this.handleChange}
+                  id={"email"}
+                  required={true}
+                />
+              </div>
+            </div>
+            <div className="field">
+              <label className="label" htmlFor={"message"}>
+                Message
+              </label>
+              <div className="control">
+                <textarea
+                  className="textarea"
+                  name={"message"}
+                  onChange={this.handleChange}
+                  id={"message"}
+                  required={true}
+                />
+              </div>
+            </div>
+            <div className="field">
+              <button className="button is-link" type="submit">
+                Send
+              </button>
+            </div>
+          </form>
+        </div>
       </Layout>
     );
   }
