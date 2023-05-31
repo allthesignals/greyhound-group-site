99b8ae12d366a313156fec5d2b50ceda9f01ad12
Matt Gardner <wilburnforce@gmail.com>
2023-05-30 21:28:01 -0400

    Footer shows address

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,6 +6,7 @@ const Footer = class extends React.Component {
     return (
       <footer className="bg-gg-lavender p-12">
         <div className="bg-gg-light-gray w-full rounded-3xl border-gg-dark-green p-12">
+          <span> 12 NW 13th St, Suite 1 — Oklahoma City OK 73103</span>
           <img
             className="ml-auto h-16"
             src={smLogo}
